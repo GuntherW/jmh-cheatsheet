a1737b32754362863785b99d0878cca39386f613
Gunther Wittig <gunther.wittig@codecentric.de>
2021-02-05 07:21:23 +0100

    Benchmarking mit Christian

---
+++ src/main/scala/de/codecentric/wittig/jmh/View2Benchmark.scala
@@ -0,0 +1,48 @@
+package de.codecentric.wittig.jmh
+
+import org.openjdk.jmh.annotations.{Scope, State, _}
+
+case class Category(eins: Option[Int], zwei: String)
+
+@State(Scope.Thread)
+class ListState {
+  val list: List[Category] = List.tabulate(100000) { i =>
+    val modulo = i % 10
+    val eins   = if (modulo == 0) None else Some(modulo)
+    Category(eins, s"hallo $i")
+  }
+}
+
+@BenchmarkMode(Array(Mode.AverageTime, Mode.Throughput))
+@Warmup(iterations = 2)
+@Measurement(iterations = 2)
+@Fork(value = 1)
+@Threads(value = 1)
+class View2Benchmark {
+
+  @Benchmark
+  def test1(state: ListState): Seq[(Int, Int)] =
+    state.list
+      .filter(_.eins.isDefined)
+      .groupBy(_.eins.get)
+      .map { case (key, value) => key -> value.size }
+      .toList
+
+  @Benchmark
+  def test2(state: ListState): Seq[(Int, Int)] =
+    state.list.view
+      .filter(_.eins.isDefined)
+      .groupBy(_.eins.get)
+      .view
+      .mapValues(_.toList)
+      .toMap
+      .map { case (key, value) => key -> value.size }
+      .toList
+
+  @Benchmark
+  def test3(state: ListState): Seq[(Int, Int)] =
+    state.list
+      .groupBy(_.eins)
+      .collect { case (Some(key), value) => key -> value.size }
+      .toList
+}
